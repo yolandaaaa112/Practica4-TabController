53b62ac8fba7466524925344d9b37bc8d91b963f
Alan Olivares <alanolivares@alans-imac.local>
2018-09-18 20:41:48 -0500

    Initial Commit

---
+++ Practica4-TabController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Practica4-TabController
+//
+//  Created by Alan Olivares on 18/09/18.
+//  Copyright © 2018 Alan Olivares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
